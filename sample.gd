83f9df7a3762044444e855acdc46bab494bb2837
akartikus <sitraka.razafimamy@gmail.com>
2024-08-02 00:12:36 +0200

    Add avatar

--- sample.gd
+++ sample.gd
@@ -1,10 +1,23 @@
 extends Control
 
-var is_playing : bool
 var sounds = []
+var words = [
+	"maman",
+	"papa",
+	"assis",
+	"bebe",
+	"dodo",
+	"doudou",
+	"lapin",
+	"manger",
+	"mimi",
+	"oui"
+]
 var current = 0
+var last = 1
 
 @onready var nick_label : Label = get_node("%Nickname")
+@onready var avatar : TextureRect = get_node("%Avatar")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,18 +31,28 @@ func _ready():
 	sounds.append(preload("res://assets/audio/manger.wav"))
 	sounds.append(preload("res://assets/audio/mimi.wav"))
 	sounds.append(preload("res://assets/audio/oui.wav"))
+	
 	randomize()
+	_randomize_word()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Button.disabled = $AudioStreamPlayer2D.is_playing()
+	$PlayerInfoBox/PlayBtn.disabled = $AudioStreamPlayer2D.is_playing()
 
 func _on_button_pressed():
 	if !$AudioStreamPlayer2D.is_playing():
 		$AudioStreamPlayer2D.stream = sounds[current]
 		$AudioStreamPlayer2D.play();
-		nick_label.text = str(is_playing)
-
+		
 
 func _on_button_2_pressed():
+	_randomize_word()
+	
+func _randomize_word():
 	current = randi() % sounds.size() # Replace with function body.
+	if current == last:
+		current = randi() % sounds.size()
+	last = current
+	
+	nick_label.text = str(words[current])	
+	avatar.texture = load("res://assets/avatar/" + words[current] +".webp")
